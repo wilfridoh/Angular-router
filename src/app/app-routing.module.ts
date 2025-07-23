bcc266bdb2d59f9086924fd8b0fe2d14b8a47d63
claudihidal <claudihidal@gmail.com>
2025-07-23 00:48:36 -0500

    ✨ Clase 2 Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component:HomeComponent
+  },
+  {
+    path: 'category',
+    component:CategoryComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
