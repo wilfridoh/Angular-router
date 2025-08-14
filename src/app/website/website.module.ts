cdc91afe9a753dc6355e9219de0db144e26306ba
claudihidal <claudihidal@gmail.com>
2025-08-13 23:55:10 -0500

    actualizacion de los modulos, creacion de mi componente compartido

---
+++ src/app/website/website.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WebsiteRoutingModule } from './website-routing.module';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { SwiperModule } from 'swiper/angular';
+import { LayoutComponent } from './components/layout/layout.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    NavComponent,
+    HomeComponent,
+    CategoryComponent,
+    MyCartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent,
+    ProductDetailComponent,
+    LayoutComponent,
+  ],
+  imports: [CommonModule, WebsiteRoutingModule, SwiperModule, SharedModule],
+})
+export class WebsiteModule {}
